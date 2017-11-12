4f23491708dbd10d0b4fc8061250b15a906f57f8
Like Zheng <keithleaves@gmail.com>
2017-11-12 17:09:56 -0500

    hello world

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,36 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './index.js',
 
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+
+  module: {
+    rules: [{
+      test: /\.(js|jsx|mjs)$/,
+      loader: 'babel-loader',
+    }]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './index.html',
+    }),
+  ],
+
+  devServer: {
+    contentBase: './dist',
+    compress: true,
+    hot: true,
+    inline: true,
+    host: 'localhost',
+    port: 8080,
+    watchContentBase: false,
+  }
 
 };
